5c5afc29321cc17279e8068b8b6128bcfda9a41b
Fredis <fredericstojicevic@gmail.com>
2018-12-04 19:24:42 +0100

    test

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,8 +1,6 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-
-
 Bot.on :optin do |optin|
   @user_id = optin.sender
 end
@@ -10,9 +8,9 @@ end
 puts "///// USER ID /////"
 puts @user_id
 
-@response = HTTParty.get("https://graph.facebook.com/#{@user_id}?fields=first_name,last_name,profile_pic&access_token=#{ENV['ACCESS_TOKEN']}")
-puts '///// RESPONSE /////'
-puts response
+# @response = HTTParty.get("https://graph.facebook.com/#{@user_id}?fields=first_name,last_name,profile_pic&access_token=#{ENV['ACCESS_TOKEN']}")
+# puts '///// RESPONSE /////'
+# puts response
 
 Bot.on :message do |message|
   message.reply(text: message.id)
@@ -24,6 +22,4 @@ Bot.on :message do |message|
   puts 'Message Seq :'
   puts message.seq
   puts '///////////'
-
-
 end
