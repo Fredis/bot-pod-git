c78bb0ebc72dbda8e0b432eec0bc1108236d9c4a
Fredis <fredericstojicevic@gmail.com>
2018-12-04 19:05:00 +0100

    test

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.1'
 
 gem 'facebook-messenger'
 gem 'dotenv-rails'
+gem 'httparty'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,6 +1,8 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
+response = HTTParty.get('https://graph.facebook.com/2134764759913485?fields=first_name,last_name,profile_pic&access_token=EAAPSxjRGx7UBAO3MrWF96h5raDyZC90L1JWJ0R7qdsMoA17eCBTzfJZALOnNRUkdigLdoI1IRPuYEXJZAwtdLxN5mTZCoA7BjZCuy0mkgN4xsj16zASr5KhVpNKbjFqXGgmf9nBT4nr9I2naj5gjlhWN3WPHD7IBGULV71293swZDZD')
+
 Bot.on :message do |message|
   message.reply(text: message.id)
   puts '///////////'
@@ -13,9 +15,4 @@ Bot.on :message do |message|
   puts '///////////'
 
 
-end
-
-
-Bot.on :messages do |messageahahahah|
-  messageahahahah.reply(text: 'yoyoyoyo')
 end
