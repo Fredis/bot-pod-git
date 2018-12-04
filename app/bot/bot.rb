45417377fde34c2820f1fe80f9e963a581110013
Fredis <fredericstojicevic@gmail.com>
2018-12-04 17:58:46 +0100

    test

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,4 +4,27 @@ include Facebook::Messenger
 Bot.on :message do |message|
   message.reply(text: message.id)
   puts '///////////'
+  puts 'Message Sender :' + message.sender
+  puts '///////////'
+  puts '///////////'
+  puts 'Message Seq :' + message.seq
+  puts '///////////'
+
+	message.reply(
+	 attachment: {
+	   type: 'template',
+	   payload: {
+	     template_type: 'button',
+	     text: 'Human, do you like me?',
+	     buttons: [
+	       { type: 'postback', title: 'Yes', payload: 'HARMLESS' },
+	       { type: 'postback', title: 'No', payload: 'EXTERMINATE' }
+	     ]
+	   }
+	 }
+	)
+
+
 end
+
+
