502ada565330b69da6bffe4f44b5e972df59574b
Fredis <fredericstojicevic@gmail.com>
2018-12-04 17:46:33 +0100

    test

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,5 +2,7 @@ require 'facebook/messenger'
 include Facebook::Messenger
 
 Bot.on :message do |message|
-  message.reply(text: user.id)
+  message.reply(text: message.id)
 end
+
+puts '///////////'
