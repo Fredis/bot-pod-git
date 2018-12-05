e9238c33631647dd3ea188dc38010ef46f5bfb51
Fredis <fredericstojicevic@gmail.com>
2018-12-05 01:20:35 +0100

    test

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -6,14 +6,14 @@ Bot.on :message do |message|
   puts '///////////'
   puts 'Message Sender :'
   puts message.sender
-  sender_id = message.sender.to_s
+  @sender_id = message.sender[:id]
   puts '///////////'
   puts '///////////'
   puts 'Message Seq :'
   puts message.seq
   puts '///////////'
 
-	@response = HTTParty.get("https://graph.facebook.com/#{sender_id}?fields=first_name,last_name,profile_pic&access_token=#{ENV['ACCESS_TOKEN']}")
+	@response = HTTParty.get("https://graph.facebook.com/#{@sender_id}?fields=first_name,last_name,profile_pic&access_token=#{ENV['ACCESS_TOKEN']}")
 	puts '///// RESPONSE /////'
 	puts @response
   
